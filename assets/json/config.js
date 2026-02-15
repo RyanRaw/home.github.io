37acf484e982da63f57f03f1bdc57d8e9d501ddb
dmego <dmego@users.noreply.github.com>
2026-02-15 03:18:27 +0000

    Update daily theme & weekly stats: rest

--- assets/json/config.js
+++ assets/json/config.js
@@ -1,7 +1,7 @@
 window.WAKATIME_CONFIG = {
-  "date": "2026-02-13",
-  "hours": 6.4,
-  "theme_name": "focused",
-  "theme_display": "专注日",
-  "updated_at": "2026-02-14T02:48:40.439Z"
+  "date": "2026-02-14",
+  "hours": 0,
+  "theme_name": "rest",
+  "theme_display": "休息日",
+  "updated_at": "2026-02-15T03:18:25.291Z"
 };
